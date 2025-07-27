1f4767303bed47728ba5d98c84eae8ed4c99725b
imassenda <salmaimassenda2023@gmail.com>
2025-07-27 14:47:53 +0100

    set up phone sms authentification

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -21,7 +21,7 @@ export default function SignInPage(){
             </form>
             {/*    */}
             <form>
-                <button className="google_button" formAction={handelGoogleSignIn} >
+                <button className="google_button mb-3" formAction={handelGoogleSignIn} >
                     <svg width="18" height="18" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
                         <path fill="currentColor" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
@@ -32,6 +32,15 @@ export default function SignInPage(){
                     Se connecter avec Google
                 </button>
             </form>
+            {/*    */}
+
+            <button className="phone_button"  >
+                <a href="/phoneLogin">
+                    Phone Authentication
+                </a>
+
+            </button>
+
 
             <div style={{ textAlign: "center", marginTop: "1rem" }}>
                 <span style={{ color: "#666" }}>Vous n'avez pas un compte ? </span>
@@ -39,6 +48,7 @@ export default function SignInPage(){
                     S'inscrire
                 </a>
             </div>
+
         </div>
     )
 
